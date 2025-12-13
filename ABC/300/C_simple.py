aaa7573f5fb4c1b223a7e5e1a2960de5d7c7905b
fafafa <wvphfk6yk5@privaterelay.appleid.com>
2025-12-14 00:13:20 +0900

    Add grid analysis and cross length calculation
    Implement a function to analyze grid patterns and calculate cross lengths.

---
+++ ABC/300/C_simple.py
@@ -0,0 +1,39 @@
+h,w=map(int,input().split())
+grid=[input().strip() for _ in range(h)]
+mn=min(h,w)#max_n
+n=0
+s=[0]*(mn+1)
+
+#バツの長さはdかどうか
+def jadge_cross(y,x,d):
+  shift=[[d,d],[d,-d],[-d,d],[-d,-d]]
+  for dy,dx in shift:
+    ny=y+dy
+    nx=x+dx
+    if not(0<=nx<w and 0<=ny<h):
+      return False
+    if grid[ny][nx] == ".":
+      return False
+  return True
+
+#どんくらいdを伸ばせるか
+def max_d(y,x):
+  d=1
+  while d <= mn:
+    if jadge_cross(y,x,d):
+      d+=1
+    else:
+      break
+  return d-1
+
+#全探索((0,x)と(y,0)は中心となり得ないので除外)
+for y in range(1,h):
+  for x in range(1,w):
+    if grid[y][x] == ".":
+      continue
+    n=max_d(y,x)
+    if n>0:
+      s[n]+=1
+
+print(*s[1:mn+1])
+  
